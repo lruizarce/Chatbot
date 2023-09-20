ffa6b0ba89aeb2abece2eab5dff4bb6cf21e5424
lruizarce <messiluisruiz@gmail.com>
2023-09-19 18:19:14 -0600

    configured ports

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+# uvicorn main:app
+# uvicorn main:app --reload
 
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai   
+
+# Custom Functions Import
+
+
+# initate App
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "My name is Jim"}
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:4174",
+    "http://localhost:3000",
+]
+
+
+# CORS - Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# check health
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
